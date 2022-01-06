d873be80008405210cc52d17bb6cbc96e9827d3f
NuLLrb <dylan@llunull.com>
2022-01-06 01:54:18 -0500

    fixed recipe list items finally!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function copy_function() {
     var copyRecipe = document.getElementById("copy-test");
-    
+
     navigator.clipboard.writeText(copyRecipe.value);
 
     alert("Copied the recipe's ingredients!")
